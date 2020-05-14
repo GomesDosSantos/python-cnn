40d97a665b2e2d54de64622b7724659ddf34729c
AbnerErnaniADSFatec <abiner.anjos@gmail.com>
2020-05-14 11:54:00 -0300

    [#4] Abstraindo o catalogo de imagens

--- services/georasters.py
+++ services/georasters.py
@@ -1,98 +1,101 @@
 import sys
+import os
 import wget
 import matplotlib.image as mpimg
 from osgeo import gdal
 from datetime import datetime, timedelta
 from PIL import Image
 
+import rasterio
+import rasterio.features
+import rasterio.warp
+
 # Criar uma classe para a abstração da busca por imagens
 class Georaster:
-    def __init__(self, path):
-        self.home = str(path)
-
-    def getGeoRaster(self, date, band, file=False, download=True, convert=True):
-        data_datetime = datetime.strptime(date, '%Y-%m-%d %Hh:%Mm:%Ss')
-        mask = (
+    def __init__(self, date=None, band=None, proj=None):
+        self.path = "data/input/train"
+        self.date_time =  datetime.strptime(date, "%Y-%m-%d %Hh:%Mm:%Ss")
+        self.mask = (
             "/clip_{year}{month}{day}T{hour}{minute}{second}_Sigma0_{band}_db"
         ).format(
-            year=data_datetime.strftime("%Y"),
-            month=data_datetime.strftime("%m"),
-            day=data_datetime.strftime("%d"),
-            hour=data_datetime.strftime("%H"),
-            minute=data_datetime.strftime("%M"),
-            second=data_datetime.strftime("%S"),
+            year=self.date_time.strftime("%Y"),
+            month=self.date_time.strftime("%m"),
+            day=self.date_time.strftime("%d"),
+            hour=self.date_time.strftime("%H"),
+            minute=self.date_time.strftime("%M"),
+            second=self.date_time.strftime("%S"),
             band=str(band).upper()
         )
-        if not file:
-            url = (
-                "http://www.dpi.inpe.br/agricultural-database/lem/dados/cenas/Sentinel1" +
-                "/{year}{month}{day}_S1A" +
-                mask + '.tif'
-            ).format(
-                year=data_datetime.strftime("%Y"),
-                month=data_datetime.strftime("%m"),
-                day=data_datetime.strftime("%d"),
+        self.band = band
+        self.projection = "EPSG:{}".format(str(proj))
+        self.url = "http://www.dpi.inpe.br/agricultural-database/lem/dados/cenas/Sentinel1"
+        self.base = (
+            "http://www.dpi.inpe.br/agricultural-database/lem/dados/cenas/Sentinel1" +
+            "/{year}{month}{day}_S1A" +
+            self.mask + ".tif"
+        ).format(
+            year=self.date_time.strftime("%Y"),
+            month=self.date_time.strftime("%m"),
+            day=self.date_time.strftime("%d"),
+        )
+        self.geotiff_path = self.path
+        self.jpg_path = self.path
+        self.georaster = None
+        self.geom = None
+        self.jpg = None
+
+    def openRemoteFile(self):
+        # try:
+        if True:
+            self.georaster = rasterio.open('/vsicurl/{}'.format(self.base))
+            mask = self.georaster.dataset_mask()
+            self.geom = []
+            for geom, val in rasterio.features.shapes(mask, transform=self.georaster.transform):
+                self.geom.append(rasterio.warp.transform_geom(self.georaster.crs, self.projection, geom, precision=6))
+            return True
+        # except:
+            # return False
+
+    def downloadRemoteFile(self):
+        file = self.path + self.mask + ".tif"
+        # try:
+        if True:
+            wget.download(
+                self.base,
+                file
             )
-            if download:
-                wget.download(
-                    url,
-                    'data/input/train' + mask + '.tif'
-                )
-            if convert:
-                options_list = [
-                    '-ot Byte',
-                    '-of JPEG',
-                    '-b 1',
-                    '-scale'
-                ]
-                options_string = " ".join(options_list)
-                try:
-                    gdal.Translate(
-                        'data/input/train' + mask + '.jpg',
-                        'data/input/train' + mask + '.tif',
-                        options=options_string
-                    )
-                    return {
-                        "base" : url,
-                        "path" : 'data/input/train' + mask + '.jpg',
-                        "datetime" : data_datetime,
-                        "georaster" : gdal.Open('/vsicurl/{}'.format(url)),
-                        "image/jpg" : Image.open('data/input/train' + mask + '.jpg')
-                    }
-                except ValueError:
-                    return {
-                        "base" : url,
-                        "path" : 'data/input/train',
-                        "datetime" : data_datetime,
-                        "georaster" : gdal.Open('/vsicurl/{}'.format(url)),
-                        "image/jpg" : None
-                    }
-            else:
-                return {
-                    "base" : url,
-                    "path" : 'data/input/train',
-                    "datetime" : data_datetime,
-                    "georaster" : gdal.Open('/vsicurl/{}'.format(url)),
-                    "image/jpg" : None
-                }
-        else:
-            file = (
-                "data/input/train" +
-                mask
+            self.geotiff_path = file
+            return True
+        # except:
+        #     return False
+
+    def convertFileToJPG(self):
+        options_list = [
+            '-ot Byte',
+            '-of JPEG',
+            '-b 1',
+            '-scale'
+        ]
+        options_string = " ".join(options_list)
+        root = os.path.dirname(os.path.abspath('georasters'))
+        try:
+            gdal.Translate(
+                root + "/" + self.path + self.mask + '.jpg',
+                root + "/" + self.path + self.mask + '.tif',
+                options=options_string
             )
-            if convert:
-                return {
-                    "base" : file,
-                    "path" : 'data/input/train',
-                    "datetime" : data_datetime,
-                    "georaster" : gdal.Open(file + '.tif'),
-                    "image/jpg" : Image.open('data/input/train' + mask + '.jpg')
-                }
-            else:
-                return {
-                    "base" : file,
-                    "path" : 'data/input/train',
-                    "datetime" : data_datetime,
-                    "georaster" : gdal.Open(file + '.tif'),
-                    "image/jpg" : None
-                }
+            self.jpg_path = self.path + self.mask + '.jpg'
+            self.jpg = Image.open(self.jpg_path)
+            return True
+        except:
+            return False
+
+    def openLocalFile(self):
+        try:
+            self.georaster = rasterio.open(self.mask + '.tif')
+            mask = self.georaster.dataset_mask()
+            self.geom = []
+            for geom, val in rasterio.features.shapes(mask, transform=self.georaster.transform):
+                self.geom.append(rasterio.warp.transform_geom(self.georaster.crs, self.projection, geom, precision=6))
+        except:
+            return False
